9525affef8086c69f4188cf88d04b7b5bfc5c6dd
Vivekanandan (Vivek) Balasubramanian <vivek.balasubramanian@rutgers.edu>
2017-07-12 16:25:15 -0400

    Update gsissh_setup_stampede_ubuntu_xenial.sh

--- misc/gsissh_setup_stampede_ubuntu_xenial.sh
+++ misc/gsissh_setup_stampede_ubuntu_xenial.sh
@@ -7,13 +7,13 @@ sudo apt-get update
 sudo apt-get install globus-proxy-utils globus-simple-ca
 
 ## Installing "ca-policy-egi-core" - 1
-cd
-mkdir tmp
-cd tmp
-wget http://www.globus.org/ftppub/gt6/installers/repo/globus-toolkit-repo%5flatest%5fall.deb
-dpkg -i globus-toolkit-repo_latest_all.deb
-sudo apt-get update
-sudo apt-get install gsi-openssh-clients globus-data-management-client myproxy globus-proxy-utils globus-simple-ca ca-certificates ca-digicertassuredidrootca-root ca-digicertgridca-1-classic ca-digicertgridca-1g2-classic-2015 ca-digicertgridrootca-root ca-digicertgridtrustca-classic ca-digicertgridtrustcag2-classic globus-gsi-cert-utils-progs libglobus-gsi-cert-utils0 ssl-cert 
+# cd
+# mkdir tmp
+# cd tmp
+# wget http://www.globus.org/ftppub/gt6/installers/repo/globus-toolkit-repo%5flatest%5fall.deb
+# dpkg -i globus-toolkit-repo_latest_all.deb
+# sudo apt-get update
+# sudo apt-get install gsi-openssh-clients globus-data-management-client myproxy globus-proxy-utils globus-simple-ca ca-certificates ca-digicertassuredidrootca-root ca-digicertgridca-1-classic ca-digicertgridca-1g2-classic-2015 ca-digicertgridrootca-root ca-digicertgridtrustca-classic ca-digicertgridtrustcag2-classic globus-gsi-cert-utils-progs libglobus-gsi-cert-utils0 ssl-cert 
 
 ## Installing "ca-policy-egi-core" - 2
 curl https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3 | sudo apt-key add -
